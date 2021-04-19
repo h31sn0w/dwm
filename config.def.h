d2d7872ad4b1c45aaaecc80430c7d035da68dffe
h31sn0w <h31sn0w@gmail.com>
2021-04-19 23:56:08 +0200

    colors name

--- config.def.h
+++ config.def.h
@@ -69,12 +69,12 @@ static const char *termcmd[]  = { "st", NULL };
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-       { "normbgcolor",        STRING,  &normbgcolor },
-       { "normbordercolor",    STRING,  &normbordercolor },
-       { "normfgcolor",        STRING,  &normfgcolor },
-       { "selbgcolor",         STRING,  &selbgcolor },
-       { "selbordercolor",     STRING,  &selbordercolor },
-       { "selfgcolor",         STRING,  &selfgcolor },
+       { "color0",             STRING,  &normbgcolor },
+       { "color0",             STRING,  &normbordercolor },
+       { "color4",             STRING,  &normfgcolor },
+       { "color4",             STRING,  &selbgcolor },
+       { "color8",             STRING,  &selbordercolor },
+       { "color0",             STRING,  &selfgcolor },
        { "borderpx",           INTEGER, &borderpx },
        { "snap",               INTEGER, &snap },
        { "showbar",            INTEGER, &showbar },
